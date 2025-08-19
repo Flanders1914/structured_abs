1fbec4cb6c30947f3073a7289c111706cf117f44
ruizhiqian <ruizhiqian1@gmail.com>
2025-08-19 16:00:35 -0400

    modified:   config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ BEGIN_YEAR = 2024
 END_YEAR = 2025
 
 # NCBI API key, this api key can increase the rate limit to 10 requests per second
-NCBI_API_KEY = "55bdd0f60ffcb18619fe20176bdd96ad4809"
+NCBI_API_KEY = ""
 BATCH_SIZE = 8000 # max 10000
 
 FILE_SUFFIX = ""
